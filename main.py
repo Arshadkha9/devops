3ec7c9ce0a96045555cbce0be2981635860e4d96
Mohd Arshad Khan <osndl.mohdak@aicofindia.com>
2026-02-16 16:17:40 +0530

    first push

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to devops"}
+
+
+@app.get("/ok")
+def health_check():
+    return {"status": "all ok"}
